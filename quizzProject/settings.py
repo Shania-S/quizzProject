ec44a170d605251f798616940ebbe52c597351f3
Shania-star <64330516+Shania-star@users.noreply.github.com>
2023-02-05 16:31:22 +0100

    Update settings.py

--- quizzProject/settings.py
+++ quizzProject/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'quizzdb',
         'USER': 'shaniasta',
         'PASSWORD': 'shaniasta',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
